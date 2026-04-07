5bff686d84cb8c680a794bdf1606b2b97107f2d5
balaji-nirmit <nirmitpratapsingh11@gmail.com>
2026-04-07 18:39:30 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react()].filter(Boolean),
+export default defineConfig({
+  // SERVER REMOVED - Safe to do!
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    dedupe: ["react", "react-dom"],
   },
-}));
+});
